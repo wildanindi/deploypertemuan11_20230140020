5d6cf34e875bba2f6f172a682a553537b0aa70f6
wildanindi2@gmail.com <wildanindi2@gmail.com>
2026-05-19 14:15:44 +0700

    Menambah Code model profile

--- pertemuan11/src/main/java/com/deploy/pertemuan11/model/Profile.java
+++ pertemuan11/src/main/java/com/deploy/pertemuan11/model/Profile.java
@@ -1,8 +1,6 @@
 package com.deploy.pertemuan11.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,5 +15,10 @@ import lombok.NoArgsConstructor;
 
 public class Profile {
     @Id
-    private String
+    private String id;
+    private String nama;
+    private String alamat;
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
 }
